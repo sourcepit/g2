e41d8d37fe3dc18f74db1245f1f69a607ead20c8
Bernd Vogt <bernd.vogt@sourcepit.org>
2013-06-26 21:58:39 +0200

    Create ResourceUtils + ResourceUtilsTest

---
+++ tests/org.sourcepit.g2.derivation.tests/src/org/sourcepit/g2/derivation/tests/AllContinuousTests.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 2013 Sourcepit.org contributors and others. All rights reserved. This program and the accompanying
+ * materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.sourcepit.g2.derivation.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+import org.sourcepit.g2.derivation.ResourceUtilsTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ResourceUtilsTest.class })
+public class AllContinuousTests
+{
+
+}
